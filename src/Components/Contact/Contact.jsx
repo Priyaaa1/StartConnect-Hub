3609f1ed6848f39b337a13ab5c67788dae23f339
Priya <priyamaheshwari183@gmail.com>
2024-04-07 01:15:16 +0530

    added contact section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,8 +4,36 @@ import msg_icon from '../../assets/msg-icon.png'
 import mail_icon from '../../assets/mail-icon.png'
 import phone_icon from '../../assets/phone-icon.png'
 import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "5759d7fc-28f1-473a-a904-ab8d5f981280");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+
   return (
     <div className='contact'>
         <div className='contact-col'>
@@ -16,6 +44,18 @@ const Contact = () => {
                 <li><img src={phone_icon} alt=''></img>+91 1234567890</li>
                 <li><img src={location_icon} alt=''></img>Address xyz</li>
             </ul>
+        </div>
+        <div className='contact-col'>
+          <form onSubmit={onSubmit}>
+            <label>Your name</label>
+            <input type='text' name='name' placeholder='Enter your name' required></input>
+            <label>Phone number</label>
+            <input type='tel' name='phone' placeholder='Enter your phone' required></input>
+            <label>Write your message here</label>
+            <textarea name='message' rows='6' placeholder='Enter your message' required></textarea>
+            <button type='submit' className='btn dark-btn'>Submit <img src={white_arrow}></img></button>
+          </form>
+          <span>{result}</span>
         </div>
     </div>
   )
