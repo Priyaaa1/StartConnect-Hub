702f327b1a31b4197a95bcd7a8d07312c854f3ea
Priya <priyamaheshwari183@gmail.com>
2024-04-08 02:29:27 +0530

    added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Title from './Components/Title/Title'
 import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <About/>
         <Title subTitle='Contact Us' title='Get in Touch'/>
         <Contact/>
+        <Footer/>
       </div>
     </div>
   )
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p> ©2024 StartConnect-Hub. All rights reserved.</p>
+        <ul>
+            <li>Terms of Services</li>
+            <li>Privacy Policy</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Footer
