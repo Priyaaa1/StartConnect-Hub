839eaf29fb91a48260917429174cc7c64227cbc6
Shashank Patil <135035523+ShashankMPatil@users.noreply.github.com>
2024-05-10 18:00:13 +0530

    fixed navbar, footer, login and signup

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-  return (
-    <div className='footer'>
-        <p> ©2024 StartConnect-Hub. All rights reserved.</p>
-        <ul>
-            <li>Terms of Services</li>
-            <li>Privacy Policy</li>
-        </ul>
-    </div>
-  )
-}
+	return (
+		<div className="footer">
+			<div className="footer-content">
+				<span>©2024 StartConnect-Hub. All rights reserved.</span>
+			</div>
+			<ul>
+				<li>Terms of Services</li>
+				<li>Privacy Policy</li>
+			</ul>
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,24 +1,38 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import './Login.css'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    console.log('Logging in with email:', email, 'and password:', password);
-  };
+	const handleLogin = () => {
+		console.log("Logging in with email:", email, "and password:", password);
+	};
 
-  return (
-    <div className='login-container'>
-      <h2>Login</h2>
-      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={handleLogin}>Login</button>
-      <p>Don't have an account? <NavLink to="/signup">Sign up</NavLink></p>
-    </div>
-  );
+	return (
+		<div className="login-outerContainer">
+			<div className="login-container">
+				<h2>Login</h2>
+				<input
+					type="email"
+					placeholder="Email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button onClick={handleLogin}>Login</button>
+				<p>
+					Don't have an account? <NavLink to="/signup">Sign up</NavLink>
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
