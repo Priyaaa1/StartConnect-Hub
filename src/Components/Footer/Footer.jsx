c494aaa9804994d739077cb32352082131e67d59
Varnika Dayal <varnikadayal60@gmail.com>
2024-05-14 16:56:53 +0530

    Conflicting Codes Resolved

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,19 +2,7 @@ import React from "react";
 import "./Footer.css";
 const Footer = () => {
 	return (
-		<div className="footer">
-			<div className="footer-content">
-				<span>©2024 StartConnect-Hub. All rights reserved.</span>
-			</div>
-			<ul>
-				<li>Terms of Services</li>
-				<li>Privacy Policy</li>
-			</ul>
-			<ul> 
-			    <li><a href="https://github.com/Priyaaa1/StartConnect-Hub"><img className="Footer-img" src="github-sign.png"></img></a></li>
-			    <li><a href=""><img className ="Footer-img" src="linkedin.png"></img></a></li>
-			</ul>
-		</div>
+		<></>
 	);
 };
 
