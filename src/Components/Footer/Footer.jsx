4f745cc8461ec0f7229e5a7213a705a3364b98bf
Varnika Dayal <varnikadayal60@gmail.com>
2024-05-14 17:12:01 +0530

    Resolved Conflicts new Pull Request For issue #103 and #110

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,17 +1,6 @@
 import React from "react";
 import "./Footer.css";
 const Footer = () => {
-	return (
-		<div className="footer">
-			<div className="footer-content">
-				<span>©2024 StartConnect-Hub. All rights reserved.</span>
-			</div>
-			<ul>
-				<li>Terms of Services</li>
-				<li>Privacy Policy</li>
-			</ul>
-		</div>
-	);
 };
 
 export default Footer;
