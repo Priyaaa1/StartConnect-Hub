9b49c2499c6f7cff11d64f942e3477a6f5bbcade
Naman Parashar <namandaksh2005@gmail.com>
2024-06-05 14:07:39 +0530

    Resolved merge conflicts in Footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import { useLocation } from "react-router-dom";
 const Footer = () => {
   const location = useLocation();
   const [sticky, setSticky] = useState(false);
-  
+
   useEffect(() => {
     window.addEventListener("scroll", () => {
       window.scrollY > 200 ? setSticky(true) : setSticky(false);
@@ -50,9 +50,9 @@ const Footer = () => {
       duration: 500,
     });
   };
+
   return (
     <footer className="footer">
-
       <div className="grid-container">
         <div
           className="row"
@@ -60,36 +60,33 @@ const Footer = () => {
             display: "flex",
             justifyContent: "space-between",
             alignItems: "center",
-
             letterSpacing: 0.5,
           }}
         >
           <div className="footer-col">
             <div className="needHelpCss">
-              <div style={{ fontSize: 20, textTransform: "uppercase" , marginTop: 15}}>
+              <div style={{ fontSize: 20, textTransform: "uppercase", marginTop: 15 }}>
                 Need Help
               </div>
               <div style={{ marginTop: 10 }}>
                 <div>
-                  <NavLink to="/#contact" onClick={() => scrollToContact()}>
-                      Contact Us
+                  <NavLink to="/#contact" onClick={scrollToContact}>
+                    Contact Us
                   </NavLink>
                 </div>
                 <div>
-                  <NavLink to="/#accordian" onClick={() => scrollToFAQ()}>
+                  <NavLink to="/#accordian" onClick={scrollToFAQ}>
                     FAQ's
                   </NavLink>
                 </div>
                 <div>
-                  <NavLink to="/#tesimonials" onClick={() => scrollToTestimonials()}>
+                  <NavLink to="/#testimonials" onClick={scrollToTestimonials}>
                     Testimonials
                   </NavLink>
                 </div>
-                
                 <div className="foot">Career</div>
                 <div className="foot">Sitemap</div>
-                </div>
-             
+              </div>
             </div>
           </div>
 
@@ -98,17 +95,19 @@ const Footer = () => {
               <div style={{ fontSize: 20, textTransform: "uppercase" }}>
                 Company
               </div>
-{/* <div> */}
               <div style={{ marginTop: 10 }}>
                 <div>
-                <NavLink to="/#about" onClick={() => scrollToAbout()}>
-                  About Us
-                </NavLink>
+                  <NavLink to="/#about" onClick={scrollToAbout}>
+                    About Us
+                  </NavLink>
                 </div>
                 <div className="foot">The StartConnect-Hub Blog</div>
-                <div className="foot">Collaboration</div>
-                <div className="foot">Media</div>
-                
+                <div className="foot">
+                  <a href="https://github.com/Priyaaa1/StartConnect-Hub">
+                    Collaboration
+                  </a>
+                </div>
+                <div className="foot">Media</div> 
               </div>
             </div>
           </div>
@@ -118,14 +117,13 @@ const Footer = () => {
               <div style={{ fontSize: 20, textTransform: "uppercase" }}>
                 More info
               </div>
-
               <div style={{ marginTop: 10 }}>
-                <div><NavLink to="/termsandconditions">
-                Terms and Conditions
-                </NavLink>
-                 </div>
                 <div>
-                  <NavLink to ="/privacypolicy">Privacy Policy</NavLink></div>
+                  <NavLink to="/termsandconditions">Terms and Conditions</NavLink>
+                </div>
+                <div>
+                  <NavLink to="/privacypolicy">Privacy Policy</NavLink>
+                </div>
                 <div>
                   <NavLink to="/visionandmission">Mission and Vision</NavLink>
                 </div>
@@ -136,7 +134,7 @@ const Footer = () => {
           <div className="footer-col">
             <div className="socialSitesCss">
               <div style={{ fontSize: 20, textTransform: "uppercase" }}>
-                let's connect
+                Let's Connect
               </div>
               <div
                 style={{
@@ -147,46 +145,43 @@ const Footer = () => {
                 }}
               >
                 <div>
-                  {" "}
                   <a
                     href="https://facebook.com/yourpage"
                     target="_blank"
-                    rel="noopener noreferrer" className="facebook"
+                    rel="noopener noreferrer"
+                    className="facebook"
                   >
-                    <i className="fab fa-facebook" style={{fontSize:"25px"}}></i>
+                    <i className="fab fa-facebook" style={{ fontSize: "25px" }}></i>
                   </a>
                 </div>
-
                 <div>
-                  {" "}
                   <a
                     href="https://twitter.com/yourpage"
                     target="_blank"
-                    rel="noopener noreferrer" className="twitter"
+                    rel="noopener noreferrer"
+                    className="twitter"
                   >
-                    <i ><svg xmlns="http://www.w3.org/2000/svg" width="25" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
-  <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
-</svg></i>
+                    <i className="fa-brands fa-square-x-twitter" style={{ fontSize: "25px" }}></i>
                   </a>
                 </div>
                 <div>
-                  {" "}
                   <a
                     href="https://linkedin.com/yourpage"
                     target="_blank"
-                    rel="noopener noreferrer" className="linkedin"
+                    rel="noopener noreferrer"
+                    className="linkedin"
                   >
-                    <i className="fab fa-linkedin" style={{fontSize:"25px"}}></i>
+                    <i className="fab fa-linkedin" style={{ fontSize: "25px" }}></i>
                   </a>
                 </div>
                 <div>
-                  {" "}
                   <a
                     href="https://instagram.com/yourpage"
                     target="_blank"
-                    rel="noopener noreferrer" className="instagram"
+                    rel="noopener noreferrer"
+                    className="instagram"
                   >
-                    <i className="fab fa-instagram" style={{fontSize:"25px"}}></i>
+                    <i className="fab fa-instagram" style={{ fontSize: "25px" }}></i>
                   </a>
                 </div>
               </div>
@@ -194,7 +189,6 @@ const Footer = () => {
           </div>
         </div>
       </div>
-
       <span className="copyright">
         © {new Date().getFullYear()} StartConnect-Hub. All rights reserved.
       </span>
