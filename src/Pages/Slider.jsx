72f8acbfaa0565758a5140aa81936c6f2eeea5c5
avanimathur <avanimathur24@gmail.com>
2024-06-12 16:27:14 +0530

    add link to startups

--- src/Pages/Slider.jsx
+++ src/Pages/Slider.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
@@ -23,7 +24,7 @@ function Slide() {
             <div className="flex flex-col items-center justify-center gap-4 p-4">
               <p className="text-xl font-semibold">{d.name}</p>
               <p className="text-center">{d.review}</p>
-              <button className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl'>Read More</button>
+              <a href={d.link} target="_blank" rel="noopener noreferrer"><button className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl'>Read More</button></a>
             </div>
           </div>
         ))}
@@ -38,27 +39,32 @@ const data = [
   {
     name: `Understanding United`,
     img: `/src/assets/startup/project-1.png`,
-    review: `Together, We Thrive: Understanding Unite`
+    review: `Together, We Thrive: Understanding Unite`,
+    link: `/Pages/Startups/UnderstandingUnited.jsx`
 },
   {
     name: `Heartfelt Ventures`,
     img: `/src/assets/startup/project-2.jpg`,
-    review: `Building Dreams, Bridging Realities`
+    review: `Building Dreams, Bridging Realities`,
+    link: `/Pages/Startups/HeartfeltVentures.jsx`
   },
   {
     name: `SheSpark Innovations`,
     img: `/src/assets/startup/project-6.png`,
-    review: `Empowering Unity, Empowering Tomorrow`
+    review: `Empowering Unity, Empowering Tomorrow`,
+    link: `/Pages/Startups/SheSparkInnovations.jsx`
   },
   {
     name: `Hope Phoenix`,
     img: `/src/assets/startup/project-4.jpg`,
-    review: `Embracing Tomorrow's Promise`
+    review: `Embracing Tomorrow's Promise`,
+    link: `/Pages/Startups/HopePhoenix.jsx`
   },
   {
     name: `Just Brew`,
     img: `/src/assets/startup/project-5.png`,
-    review: `Turning Ideas into Impact`
+    review: `Turning Ideas into Impact`,
+    link: `/Pages/Startups/JustBrew.jsx`
   },
   
 ];
