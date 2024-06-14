4094a154109f9d70d10ca82a1b85375bbe1aa426
Braj Narayan Awasthi <3083brajnarayanawasthi@gmail.com>
2024-06-14 14:20:43 +0530

    updated the Footer.jsx file

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -36,9 +36,9 @@ const Footer = () => {
           <div className='footer-2'>
             <p className='footer-section-title'>Company</p>
             <ul className='footer-links'>
-            <NavLink to="/#about" onClick={() => scrollToSection("about")}>
-            <li className='footer-link'><a href='/about'>About Us</a></li>
-            </NavLink>
+              <NavLink to="/#about" onClick={() => scrollToSection("about")}>
+                <li className='footer-link'><a href='/about'>About Us</a></li>
+              </NavLink>
               <li className='footer-link'>Blogs</li>
               <a href="https://github.com/Priyaaa1/StartConnect-Hub" target="_blank" rel="noopener noreferrer"><li className='footer-link'>Collaboration</li></a>
               <li className='footer-link'>Media</li>
@@ -47,48 +47,48 @@ const Footer = () => {
           <div className='footer-2'>
             <p className='footer-section-title'>Need Help</p>
             <ul className='footer-links'>
-            <NavLink to="/#contact" onClick={() => scrollToSection("contact")}>
-            <li className='footer-link'>Contact Us</li>
-            </NavLink>
+              <NavLink to="/#contact" onClick={() => scrollToSection("contact")}>
+                <li className='footer-link'>Contact Us</li>
+              </NavLink>
               <NavLink to="/#testimonials" onClick={() => scrollToSection("testimonials")}>
-              <li className='footer-link'>Testimonials</li>
+                <li className='footer-link'>Testimonials</li>
               </NavLink>
               <li className='footer-link'>Sitemap</li>
               <NavLink to="/#accordian" onClick={() => scrollToSection("accordian")}>
-              <li className='footer-link'>FAQ</li>
+                <li className='footer-link'>FAQ</li>
               </NavLink>
             </ul>
           </div>
           <div className='footer-2'>
             <p className='footer-section-title'>More Info</p>
             <ul className='footer-links'>
-            <NavLink to="/privacypolicy"><li className='footer-link'>Privacy Policy</li></NavLink>
+              <NavLink to="/privacypolicy"><li className='footer-link'>Privacy Policy</li></NavLink>
               <NavLink to="/termsandconditions"><li className='footer-link'>Terms & Conditions</li></NavLink>
               <NavLink to="/visionandmission"><li className='footer-link'>Vision and Mission</li></NavLink>
               <li className='footer-link'>Career</li>
             </ul>
           </div>
-      </div>
-      <div className='footer-2'>
-      <p className='footer-section-title'>Let's Connect</p>
-      <div className='footer-icons footer-icons-md'>
-      <a href="https://linkedin.com/yourpage" target="_blank" rel='noreferrer'>
-      <i className="fab fa-linkedin" style={{ fontSize: "25px" }}/>
-        </a>
-        <a href="https://github.com/Priyaaa1/StartConnect-Hub" className='github' target="_blank" rel='noreferrer'>
-          <FaGithub/>
-        </a>
-        <a href="https://facebook.com/yourpage" target="_blank" rel='noreferrer'>
-        <i className="fab fa-facebook" style={{ fontSize: "25px" }}/>
-        </a>
-        <a href="https://twitter.com/yourpage" target="_blank" rel='noreferrer'>
-        <i className="fab fa-twitter" style={{ fontSize: "25px" }}/>
-        </a>
-        <a href="https://instagram.com/yourpage" target="_blank" rel='noreferrer'>
-        <i className="fab fa-instagram" style={{ fontSize: "25px" }}/>
-        </a>
-      </div>
-      </div>
+        </div>
+        <div className='footer-2'>
+          <p className='footer-section-title'>Let's Connect</p>
+          <div className='footer-icons footer-icons-md'>
+            <a href="https://linkedin.com/yourpage" target="_blank" rel='noreferrer'>
+              <i className="fab fa-linkedin" style={{ fontSize: "25px" }} />
+            </a>
+            <a href="https://github.com/Priyaaa1/StartConnect-Hub" className='github' target="_blank" rel='noreferrer'>
+              <FaGithub />
+            </a>
+            <a href="https://facebook.com/yourpage" target="_blank" rel='noreferrer'>
+              <i className="fab fa-facebook" style={{ fontSize: "25px" }} />
+            </a>
+            <a href="https://x.com/i/flow/login?redirect_after_login=%2Fyourpage" target="_blank" rel='noreferrer'>
+              <i className="fab fa-twitter" style={{ fontSize: "25px" }} />
+            </a>
+            <a href="https://instagram.com/yourpage" target="_blank" rel='noreferrer'>
+              <i className="fab fa-instagram" style={{ fontSize: "25px" }} />
+            </a>
+          </div>
+        </div>
       </div>
       <div className='footer-bottom'>
         <div className='footer-bottom-link'>
