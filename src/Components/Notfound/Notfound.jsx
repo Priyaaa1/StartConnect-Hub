c9de07aaa2cb80f836bc88006c70805aa6068669
RamakrushnaBiswal <ramakrushnabunty@gmail.com>
2024-06-14 11:00:40 +0530

    fixed the buttons

--- src/Components/Notfound/Notfound.jsx
+++ src/Components/Notfound/Notfound.jsx
@@ -4,7 +4,7 @@ export default function Notfound(){
     return(
         <div className="notfound-container">
            <img src="./404.png" alt="Not found pic" className="notfound" />
-           <Link to={"/"} className="btn">Go to Home</Link>
+           <Link to={"/"} className="btnx" style={{textDecoration: "none"}}>Go to Home</Link>
         </div>
     )
 }
