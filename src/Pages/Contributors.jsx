edeae26824e132096c8de1eb8a85c803cb7258b2
pani2004 <debashispani61@gmail.com>
2024-07-10 00:32:23 +0530

    Added the contributors page

---
+++ src/Pages/Contributors.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Contributors.css';
+function Contributors() {
+  const [contributors, setContributors] = useState([]);
+  useEffect(() => {
+    async function Contributors() {
+      try {
+        const response = await axios.get(
+          'https://api.github.com/repos/Priyaaa1/StartConnect-Hub/contributors'
+        );
+        setContributors(response.data);
+      } catch (error) {
+        console.error('Error in fetching contributors:', error);
+      }
+    }
+    Contributors();
+  }, []);
+
+  return (
+    <div className="contributors-container">
+      <h1 className="contributors-title">Contributors</h1>
+      <div className="contributors-grid">
+        {contributors.map((contributor) => (
+          <div key={contributor.id} className="contributor-card">
+            <a
+              href={contributor.html_url}
+              className="contributor-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={contributor.avatar_url}
+                alt={contributor.login}
+                className="contributor-avatar"
+              />
+            </a>
+            <h2 className="contributor-name">{contributor.login}</h2>
+            <p className="contributor-contributions">
+              Contributions: {contributor.contributions}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Contributors;
