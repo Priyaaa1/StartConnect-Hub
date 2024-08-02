9906a89d391578be5aea2c92b8ec3baa9ebc8ee9
Priya <105431748+Priyaaa1@users.noreply.github.com>
2024-08-02 22:42:29 +0530

    Revert "Feat stripe payment invest "

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App'; // Assuming you have an App component
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import store from './app/store';
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Preloader from './Components/Preloader/Preloader.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import store from './app/store'
+import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="1039349867829-5mc9jupnfc21969mbsk1pf4vgovafdhg.apps.googleusercontent.com">
       <Provider store={store}>
-        <App /> {/* Render App instead of Preloader */}
+        <Preloader/>
       </Provider>
     </GoogleOAuthProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
