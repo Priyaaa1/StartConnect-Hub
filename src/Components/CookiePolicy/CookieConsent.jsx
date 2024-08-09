162a77ac8ce9f69f99a16e728055302fcb6eee7a
Sayyad Aslam <sayyadaslam2020@gmail.com>
2024-08-09 23:11:04 +0530

    Created CookieConsent.jsx

---
+++ src/Components/CookiePolicy/CookieConsent.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './CookieConsent.css';
+
+const CookieConsent = () => {
+    const [isVisible, setIsVisible] = useState(true);
+
+    const handleAccept = () => {
+        setIsVisible(false);
+    };
+
+    return (
+        isVisible && (
+            <div className="cookie-consent">
+                <p>At StartConnect Hub, we use cookies to provide you with a better user experience, including personalized content and advertisements.</p>
+                <div className="cookie-consent-buttons">
+                    <Link to="/privacypolicy" className="learn-more">
+                        Learn more
+                    </Link>
+                    <button onClick={handleAccept}>Accept All</button>
+                </div>
+            </div>
+        )
+    );
+};
+
+export default CookieConsent;
